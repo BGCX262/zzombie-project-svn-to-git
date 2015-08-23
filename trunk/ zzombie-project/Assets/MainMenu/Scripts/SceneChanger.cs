ce9acbd7dd93199ad9864b1d58e59ab67effcf11
Your Name <you@example.com>
2015-08-23 12:57:53 +0600

    init

---
+++ trunk/ zzombie-project/Assets/MainMenu/Scripts/SceneChanger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour {
+
+	public int mainMenu;
+	private int currentLevel;
+
+	public void Awake()
+	{
+		currentLevel = Application.loadedLevel;
+	}
+
+	public void ChangeTo(int scene)
+	{
+		Application.LoadLevel(scene);
+	}
+
+	public void Update()
+	{
+		if (currentLevel == mainMenu)
+			return;
+
+		#if CROSS_PLATFORM_INPUT
+		if (CrossPlatformInput.GetButtonDown("Cancel"))
+			Application.LoadLevel(0);
+		#else
+		if (Input.GetKey(KeyCode.Escape))
+			Application.LoadLevel(0);
+		#endif
+	}
+}
